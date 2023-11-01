f39e974efdd44a2af6129b674aeac9e332ea2652
manueru <juanperez@gcflatino.com>
2023-11-01 17:51:07 -0500

    feat(modularizartion)

--- HTML/DARK/index.js
+++ HTML/DARK/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  //   $("#wrapper").load("layout/layout.html");
+  $("#styler").load("components/styler/styler.html");
+  $("#background").load("components/background/background.html");
   $("#home").load("pages/home/home.html");
   $("#about").load("pages/about/about.html");
   $("#portfolio").load("pages/portfolio/portfolio.html");
