bfe4f2b3cc36a1f130d19d43e00f0b3382512d61
manueru <juanperez@gcflatino.com>
2023-11-01 17:01:43 -0500

    feat(pages): modularized

---
+++ HTML/DARK/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  //   $("#wrapper").load("layout/layout.html");
+  $("#home").load("pages/home/home.html");
+  $("#about").load("pages/about/about.html");
+  $("#portfolio").load("pages/portfolio/portfolio.html");
+  $("#blog").load("pages/blog/blog.html");
+  $("#contact").load("pages/contact/contact.html");
+});
---
+++ HTML/DARK/layout/layout.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#home").load("pages/home/home.html");
+  $("#about").load("pages/about/about.html");
+  $("#portfolio").load("pages/portfolio/portfolio.html");
+  $("#blog").load("pages/blog/blog.html");
+  $("#contact").load("pages/contact/contact.html");
+});
